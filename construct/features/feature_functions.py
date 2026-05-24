2a8b0ce8d3f7d8d712b69da3a5edfc44ab9b29fe
MartinAschl <martin.aschl@t-online.de>
2026-05-24 16:20:25 +0200

    added description for the wrapper file feature_functions

--- construct/features/feature_functions.py
+++ construct/features/feature_functions.py
@@ -1,3 +1,11 @@
+"""
+DATE: 24.05.2026
+NAME: MARTIN
+PURPOSE:
+In this file all functions that lie in the folder "construct/features" are stored as wrappers.
+If constructing something, by using a function: Always refer to the functions in this file and never to the functions directly.
+"""
+
 from construct.features.NAME.infer_name import _infer_name
 
 def infer_name(x1, x2):
